148545c1334aec258136b5fc1c946eca4b39f263
eyal.br <68502445+DeepVoyager253@users.noreply.github.com>
2023-05-11 17:57:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ sys.path.append(os.path.join(parent_folder_path, 'lib'))
 sys.path.append(os.path.join(parent_folder_path, 'plugin'))
 
 from flowlauncher import FlowLauncher
-import yt_dlp
+import os
+from pytube import YouTube
 from pathlib import Path
 class YoutubeDownload(FlowLauncher):
 
     def query(self, query):
-        if not query.startswith("https://www.youtube.com") or query.startswith("https://youtu.be"):
-            return
+        # if not query.startswith("https://www.youtube.com") and not query.startswith("https://youtu.be"):
+        #     return [            {
+        #         "Title": "Invalid URL!",
+        #         "SubTitle": "This is an Invalid youtube URL!",
+        #         "IcoPath": "Images/Media_Player_Windows_11_logo.png"
+        #     }]
         self.queryd = query
         return [
             {
@@ -22,7 +27,7 @@ class YoutubeDownload(FlowLauncher):
                 "IcoPath": "Images/Media_Player_Windows_11_logo.png",
                 "JsonRPCAction": {
                     "method": "download_audio",
-                    "parameters": ["{}".format(query)]
+                    "parameters": []
                 }
             },
                 {
@@ -31,7 +36,7 @@ class YoutubeDownload(FlowLauncher):
                 "IcoPath": "Images/7-player_windows_media_player_video-512.png",
                 "JsonRPCAction": {
                     "method": "download_video",
-                    "parameters": ["{}".format(query)]
+                    "parameters": []
                 }
             }
         ]
@@ -40,33 +45,35 @@ class YoutubeDownload(FlowLauncher):
         return []
 
 
-    def download_audio(self, url):
-        os.chdir(str(Path.home() / "Downloads"))
-        
-        ydl_opts = {
-            'format': 'm4a/bestaudio/best',
-            # ℹ️ See help(yt_dlp.postprocessor) for a list of available Postprocessors and their arguments
-            'postprocessors': [{  # Extract audio using ffmpeg
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'm4a',
-            }]
-        }
-            
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        os.system("explorer .")
 
+    def download_audio(self):
+        os.chdir("%USERPROFILE%")
+        url = self.queryd
+        try:
+            youtube = YouTube(url)
+            video = youtube.streams.filter(only_audio=True).first()
+            output_path = os.path.join(os.getcwd(), "downloads")
+            os.makedirs(output_path, exist_ok=True)
+            audio_file = video.download(output_path=output_path)
+            print("Audio downloaded successfully!")
+            print("File saved at:", audio_file)
+            os.system("explorer .")
+        except Exception as e:
+            print("Error:", str(e))
 
-    def download_video(self, url):
-        os.chdir(str(Path.home() / "Downloads"))
-        
-        ydl_opts = {
-            'format': 'mp4',
-            }
-            
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-        os.system("explorer .")
+    def download_video(self):
+        os.chdir("%USERPROFILE%")
+        url = self.queryd
+        try:
+            youtube = YouTube(url)
+            video = youtube.streams.filter(progressive=True, file_extension='mp4').first()
+            video_file = video.download()
+            print("Video downloaded successfully!")
+            print("File saved at:", video_file)
+            os.system("explorer .")
+        except Exception as e:
+            print("Error:", str(e))
+
 if __name__ == "__main__":
     YoutubeDownload()
 
